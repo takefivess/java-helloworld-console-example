e732382533e36da72529d0c10c2e9a04ef1df1a7
inuk <takefivess@nate.com>
2024-03-26 10:24:24 +0900

    stg commit

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -9,6 +9,7 @@ public class HelloWorld {
 		System.out.println("stg!");
 		System.out.println("stg!");
 		System.out.println("stg!");
+		System.out.println("stg!");
 
 		System.out.println("master!");
 
