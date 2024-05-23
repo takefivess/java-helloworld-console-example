84b6186a2ec1ec8af4c523d0a48cd5b26a9bd82c
inuk <takefivess@nate.com>
2024-05-23 17:08:49 +0900

    test commit

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -5,20 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("HelloWorld!");
-		System.out.println("stg!111");
-		System.out.println("stg!222");
-		System.out.println("stg!");
-		System.out.println("stg!");
-
-		System.out.println("master!111");
-		System.out.println("master!222");
-		System.out.println("master!333");
-
-		System.out.println("dev!111");
-		System.out.println("dev!222");
-		System.out.println("dev!333");
-		System.out.println("dev!");
 		System.out.println("dev!111");
 	}
 
